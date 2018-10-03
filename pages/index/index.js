040f4ec9d23a032c95b77860f92a70b67c17c409
liuwei <970403487@qq.com>
2018-10-03 15:14:05 +0800

    https://github.com/4Dark/SmallCodeWX/issues/1

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,3 +1,4 @@
+var i =0;
 Page({
   data: {
     // text:"这是一个页面"
@@ -5,23 +6,42 @@ Page({
     index: 0,
     time: '08:30',
     date: '2016-09-26',
-    items:[
-      {name:'a',value:'1'},
-      {name:'b',value:'2',checked:'true'},
-      {name:'c',value:'3'}
+    items: [{
+        name: 'a',
+        value: '1'
+      },
+      {
+        name: 'b',
+        value: '2',
+        checked: 'true'
+      },
+      {
+        name: 'c',
+        value: '3'
+      }
     ]
   },
-  switchChange:function(e){
+  bindGetFocus: function(e) {
+    console.log('get focous');
+  },
+  bindLostFocus: function(e) {
+    console.log('lost focous');
+  },
+  bindLineChange:function(e){
+    i=i+1;
+    console.log(i);
+  },
+  switchChange: function(e) {
     console.log(e.detail.value);
   },
 
-  radioChange:function(e){
+  radioChange: function(e) {
     console.log(e.detail.value)
   },
   /**
    * 监听普通picker选择器
    */
-  listenerPickerSelected: function (e) {
+  listenerPickerSelected: function(e) {
     //改变index值，通过setData()方法重绘界面
     this.setData({
       index: e.detail.value
@@ -31,7 +51,7 @@ Page({
   /**
    * 监听时间picker选择器
    */
-  listenerTimePickerSelected: function (e) {
+  listenerTimePickerSelected: function(e) {
     //调用setData()重新绘制
     debugger;
     this.setData({
@@ -42,25 +62,25 @@ Page({
   /**
    * 监听日期picker选择器
    */
-  listenerDatePickerSelected: function (e) {
+  listenerDatePickerSelected: function(e) {
     this.setDate({
       date: e.detail.value
     })
   },
 
-  onLoad: function (options) {
+  onLoad: function(options) {
     // 页面初始化 options为页面跳转所带来的参数
   },
-  onReady: function () {
+  onReady: function() {
     // 页面渲染完成
   },
-  onShow: function () {
+  onShow: function() {
     // 页面显示
   },
-  onHide: function () {
+  onHide: function() {
     // 页面隐藏
   },
-  onUnload: function () {
+  onUnload: function() {
     // 页面关闭
   }
 })
